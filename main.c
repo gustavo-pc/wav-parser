46106edca84a3078661da6ff9dc0edf8463b0dbc
Gustavo Pinheiro <gustavo-pinheiroc@outlook.com>
2016-04-06 06:09:46 -0300

    Filling Format chunk (bug on byterate)

--- main.c
+++ main.c
@@ -15,12 +15,12 @@ typedef struct wave_header {
 typedef struct format_chunk {
     char groupID[4];                            /// "fmt " - yes, with the whitespace
     unsigned chunkSize;                         ///
-    unsigned short wFormatTag;                  ///
-    unsigned short wChannels;                   ///
+    unsigned short formatTag;                   ///
+    unsigned short numChannels;                 ///
     unsigned sampleRate;                        ///
-    unsigned dwAvgBytesPerSec;                  ///
-    unsigned short wBlockAlign;                 ///
-    unsigned dwBitsPerSample;                   ///
+    unsigned avgBytesPerSec;                    ///
+    unsigned short blockAlign;                  ///
+    unsigned bitsPerSample;                     ///
 } FormatChunk;
 
 /** Struct for containing the raw audio samples and some metadata */
@@ -34,10 +34,15 @@ typedef struct data_chunk {
 #include <stdlib.h>
 #include <string.h>
 
-WaveHeader header;
 FILE *fileptr = NULL;
+WaveHeader header;
+FormatChunk format;
 
+//Function Prototypes
 void fillHeader();
+void fillFormat();
+char* lil_e_to_big_e_2(const char *input);
+char* lil_e_to_big_e_4(const char *input);
 
 int main(int argc, char** argv)
 {
@@ -54,6 +59,7 @@ int main(int argc, char** argv)
 
         fileptr = fopen(filename, "r");
         fillHeader();
+        fillFormat();
 
         fclose(fileptr);
     }
@@ -62,6 +68,8 @@ int main(int argc, char** argv)
 
 }
 
+
+/// Fills the WaveHeader Structure
 void fillHeader()
 {
     char littleEndian[4];
@@ -71,7 +79,64 @@ void fillHeader()
     fread(header.groupID, sizeof(header.groupID), 1, fileptr);
 
     fread(littleEndian, sizeof(header.fileLength), 1, fileptr);
-    header.fileLength = littleEndian[0] | (littleEndian[1]<<8) | (littleEndian[2]<<16) | (littleEndian[3]<<24);
+    header.fileLength = lil_e_to_big_e_4(littleEndian);//littleEndian[0] | (littleEndian[1]<<8) | (littleEndian[2]<<16) | (littleEndian[3]<<24);
 
     fread(header.sRiffType, 4, 1, fileptr);
+}
+
+/// Fills the FormatChunk Structure
+void fillFormat(){
+
+    char littleEndian4[4];
+    char littleEndian2[2];
+
+    // Going to format-chunk position on file, just for making sure
+    fseek(fileptr, 12, SEEK_SET);
+
+    // groupId
+    fread(format.groupID, sizeof(format.groupID), 1, fileptr);
+
+    // chunkSize
+    fread(littleEndian4, sizeof(format.chunkSize), 1, fileptr);     // Getting data in Little Endian
+    format.chunkSize = lil_e_to_big_e_4(littleEndian4);
+
+    // formatTag: '1' for LPCM
+    fread(littleEndian2, sizeof(format.formatTag), 1, fileptr);     // Getting data in Little Endian
+    format.formatTag = lil_e_to_big_e_2(littleEndian2);
+
+    // numChannels
+    fread(littleEndian2, sizeof(format.numChannels), 1, fileptr);     // Getting data in Little Endian
+    format.numChannels = lil_e_to_big_e_2(littleEndian2);
+
+    size_t bytes_read = 0;
+
+    // sampleRate
+    bytes_read = fread(littleEndian4, sizeof(format.sampleRate), 1, fileptr);     // Getting data in Little Endian
+    printf("Lidos %u byte(s)\n", bytes_read);
+    format.sampleRate = lil_e_to_big_e_4(littleEndian4);
+    printf("Sample Rate Chars: %02X %02X %02X %02X\n", littleEndian4[0], littleEndian4[1], littleEndian4[2], littleEndian4[3]);
+
+    // byteRate
+    bytes_read = fread(littleEndian4, sizeof(format.avgBytesPerSec), 1, fileptr);     // Getting data in Little Endian
+    printf("Lidos %u byte(s)\n", bytes_read);
+    format.avgBytesPerSec = lil_e_to_big_e_4(littleEndian4);
+    printf("Byte Rate Chars: %02X %02X %02X %02X\n", littleEndian4[0], littleEndian4[1], littleEndian4[2], littleEndian4[3]);
+
+     // blockAlign
+    fread(littleEndian2, sizeof(format.blockAlign), 1, fileptr);     // Getting data in Little Endian
+    format.blockAlign = lil_e_to_big_e_2(littleEndian2);
+
+     // bitsPerSample
+    fread(littleEndian2, sizeof(format.bitsPerSample), 1, fileptr);     // Getting data in Little Endian
+    format.bitsPerSample = lil_e_to_big_e_2(littleEndian2);
+}
+
+/// Returns the byte-inverted version of the input (4 bytes version)
+char* lil_e_to_big_e_4(const char *littleEndian){
+    return littleEndian[0] | (littleEndian[1]<<8) | (littleEndian[2]<<16) | (littleEndian[3]<<24);
+}
+
+/// Returns the byte-inverted version of the input (2 bytes version)
+char* lil_e_to_big_e_2(const char *littleEndian){
+    return littleEndian[0] | (littleEndian[1] << 8);
 }

34cbe0f256cddf8e67b88deb7a22dc425e68da7d
Gustavo Pinheiro <gustavo-pinheiroc@outlook.com>
2016-04-20 18:09:06 -0300

    Second version. Still in SRAM

--- wavetoheader.c
+++ wavetoheader.c
@@ -4,15 +4,21 @@
 FILE *wavptr = NULL;
 FILE *headerptr = NULL;
 
+void writeHeader();
 void readAndPrint();
 
 int main() {
     
-    wavptr = fopen("/Users/gustavo/Developer/SEMB/jumpmono.wav", "r");
+    wavptr = fopen("/Users/gustavo/Developer/SEMB/wave-parser/jump8.wav", "r");
     
-    if (wavptr == NULL) printf("Could not load the file\n");
+    if (wavptr == NULL) {
+        printf("Could not load the file\n");
+        return 404;
+    }
     
     headerptr = fopen("test.h", "w");
+    
+    writeHeader();
     
     // Doin the magic
     readAndPrint();
@@ -24,6 +30,14 @@ int main() {
     return 0;
 }
 
+void writeHeader(){
+    fprintf(headerptr, "typedef struct wavefile {\n\tuint8_t chNumber,\n\tunsigned sampleRate,\n\tuint8_t resolution,\n\tuint8_t *data,\n\tunsigned size\n} WaveFile;\n\n");
+    fprintf(headerptr, "WaveFile jump8 = {0};\n\n");
+    fprintf(headerptr, "jump8.chNumber = 1;\n");
+    fprintf(headerptr, "jump8.sampleRate = 1;\n");
+    fprintf(headerptr, "jump8.resolution = 1;\n");
+}
+
 void readAndPrint(){
     
     uint8_t buffer = 0;
@@ -31,14 +45,14 @@ void readAndPrint(){
     
     fseek(wavptr, 44, SEEK_SET);
     
-    fprintf(headerptr, "uint8_t *file = [");
+    fprintf(headerptr, "jump8.data = [");
     while(!(feof(wavptr))) {
         fread(&buffer, sizeof(uint8_t), 1, wavptr);
-        fprintf(headerptr, "0x%02X ", buffer);
-        //printf("Reading %02X\n", buffer);
+        fprintf(headerptr, "%d,", buffer);
         i++;
     }
     fseek(headerptr, -1, SEEK_CUR);
-    fprintf(headerptr, "];");
-    fprintf(headerptr, "\nint size = %d;", i);
+    fprintf(headerptr, "];\n");
+//    fprintf(headerptr, "\nint size = %d;", i);
+    fprintf(headerptr, "jump8.size = %d;\n", i);
 }

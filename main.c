79e0c295ae5a17742b29b5f2af7eb51cae100451
Gustavo Pinheiro <gustavo-pinheiroc@outlook.com>
2016-05-02 13:56:01 -0300

    Fixing .h generator

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
  Autores: Gustavo Pinheiro e Bruno Pessoa
  Data: Abril de 2016
 */
-//#include <stdio.h>
+
+#include <stdio.h>                         //For printf in Proteus
 #include <p18f4550.h>
 #include <usart.h>
 #include <delays.h>
@@ -13,71 +14,13 @@
 #pragma config MCLRE = OFF
 
 #define PPS 50
-#define LINE_HEIGHT 129
-#define BLACK " 0 "
+#define LINE_HEIGHT 51
+#define BLACK " 100 "
 #define WHITE " 255 "
 #define TESTING 0
 
 //byte chosenSamples[2879/(8000/PPS)][2];
 unsigned samplesCount = 0;
-
-void chooseSamples(){
-    int i = 0, j = 0, step = 8000/PPS;
-    byte buffer, max = 128, min = 127;
-
-    for(i=0; i < dataSize; i++) {
-        if(i%step == 0 && i != 0){
-            //plotValue(min, max);
-            max = 128;
-            min = 127;
-            samplesCount++;
-        }
-        if(data[i] < min) //MIN
-            max = data[i];
-        if(data[i] > max) // MAX
-            min = data[i];
-    }
-    printf("\n\nInt nosso: %d\n\r", (dataSize/step) );
-    printf("\nVerdadeiro: %d\n\r", samplesCount);
-}
-
-void plotValue(short int min, short int max){
-    int range = (LINE_HEIGHT - 1)/2, i = 0;
-
-    /// Normalizing between 1 and 128
-    int down = 128 - min, up = max - 127;
-
-    /// Normalizing between 1 and range
-    int down_mod = (down * range)/128;
-    int up_mod = (up * range)/128;
-
-    // Paddings
-    int padBottom = range - down_mod;
-    int padTop = range - up_mod;
-
-    //Plot Bottom Paddding
-    for (i = 0; i < padBottom; i++) {
-        printf( WHITE);
-    }
-
-    // Plot bottom value
-    for (i = 0; i < down_mod; i++) {
-        printf( BLACK);
-    }
-
-    //Center point
-    printf( BLACK);
-
-    // Plot bottom value
-    for (i = 0; i < up_mod; i++){
-        printf( BLACK);
-    }
-
-    //Plot Top Paddding
-    for (i = 0; i < padTop; i++) {
-        printf( WHITE);
-    }
-}
 
 void openSerialComm(){
     OpenUSART(USART_TX_INT_OFF  &
@@ -88,8 +31,79 @@ void openSerialComm(){
     Delay10KTCYx(1);
 }
 
+void writeHeader(){
+    printf("P2\n\r%d %d\n\r255\n\r", LINE_HEIGHT, dataSize/(sampleRate/PPS));
+}
+
+void plotValue(byte min, byte max){
+    int range = (LINE_HEIGHT - 1)/2, i = 0;
+
+    /// Normalizing between 1 and 128
+    int down = 128 - min, up = max - 127;
+
+    byte debug = 1;
+
+    /// Normalizing between 1 and range
+    int down_mod = (float) (down * range)/128;
+    int up_mod = (float) (up * range)/128;
+
+    // Paddings
+    int padBottom = range - down_mod;
+    int padTop = range - up_mod;
+    
+    if(debug){
+        printf("Gonna print %d down and %d up", down_mod, up_mod);
+    }    
+
+    //Plot Bottom Paddding
+    for (i = 0; i < padBottom; i++) {
+        if(!debug) printf(WHITE);
+    }
+
+    // Plot bottom value
+    for (i = 0; i < down_mod; i++) {
+        if(!debug) printf(BLACK);
+    }
+
+    //Center point
+    if(!debug) printf(BLACK);
+
+    // Plot bottom value
+    for (i = 0; i < up_mod; i++){
+        if(!debug) printf(BLACK);
+    }
+
+    //Plot Top Paddding
+    for (i = 0; i < padTop; i++) {
+        if(!debug) printf(WHITE);
+    }
+    printf("\n\r");
+}
+
+void chooseSamples(){
+    int i = 0, step = sampleRate/PPS;
+    byte buffer, max = 128, min = 127;
+    
+    for(i=0; i < dataSize; i++) {
+        if(i%step == 0 && i != 0){
+            plotValue(min, max);
+            //printf("Calling with: %3d, %3d\n\r", min, max);
+            max = 128;
+            min = 127;
+            samplesCount++;
+        }
+        if(data[i] < min) //MIN
+            max = data[i];
+        if(data[i] > max) // MAX
+            min = data[i];
+    }
+    //printf("\n\nInt nosso: %d\n\r", (dataSize/step) );
+    //printf("\nVerdadeiro: %d\n\r", samplesCount);;;
+}
+
 void main() {
     openSerialComm();
+    writeHeader();
     chooseSamples();
 
     while(1);
--- wavetoheader.c
+++ wavetoheader.c
@@ -10,12 +10,34 @@ void writeHeader();
 void readAndPrint();
 
 int main() {
-    wavptr = fopen("C:\\Users\\Bruno Pessoa\\Desktop\\parser\\parser_project\\jump8.wav", "r");
+    wavptr = fopen("C:\\Users\\gustavo\\Developer\\SEMB\\wav-parser\\jump8.wav", "r");
 
     if (wavptr == NULL) {
         printf("Could not load the file\n");
         return 404;
     }
+
+    ///Before running
+
+
+    //char groupID[5];
+    //groupID[4] = '\0';
+    //unsigned char buffer;
+    //int i = 0;
+
+    //while (!feof(wavptr)){
+    //    fread(&buffer, sizeof(buffer), 1, wavptr);
+    //    printf("%d: %c\t", i++, buffer);
+    //}
+
+    //fseek(wavptr, 96, SEEK_SET);
+    //fread(groupID, sizeof(groupID), 1, wavptr);
+    //printf(groupID);
+
+    //rewind(wavptr);
+
+    /// End Before running
+
 
     headerptr = fopen("wave.h", "w");
 
@@ -33,7 +55,7 @@ int main() {
 
 void writeHeader(){
     fprintf(headerptr, "typedef unsigned char byte;\n");
-    fprintf(headerptr, "byte chNumber = 1;\nshort sampleRate = 8000;\nbyte resolution = 8;\nrom nbyte data[] = {");
+    fprintf(headerptr, "byte chNumber = 1;\nshort sampleRate = 8000;\nbyte resolution = 8;\nrom byte data[] = {");
 }
 
 void readAndPrint(){
@@ -41,10 +63,11 @@ void readAndPrint(){
     uint8_t buffer = 0;
     int i = 0;
 
-    fseek(wavptr, 44, SEEK_SET);
+    fseek(wavptr, 96 + 8, SEEK_SET);
 
     while(!(feof(wavptr))) {
         fread(&buffer, sizeof(uint8_t), 1, wavptr);
+        //if(i >= 0 && i <= 40 ) printf("data[%2d]: %c 0x%02X %3d \n", i, buffer, buffer, buffer);
         fprintf(headerptr, "%d,", buffer);
         i++;
     }
